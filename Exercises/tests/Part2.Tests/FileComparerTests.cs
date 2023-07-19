6699f86826579492f628d771b23b85cbccec05a7
Astijus Arimavičius <60033715+astijusar@users.noreply.github.com>
2023-07-19 10:36:14 +0300

    Add FileComparer and FileValidator tests

---
+++ Exercises/tests/Part2.Tests/FileComparerTests.cs
@@ -0,0 +1,172 @@
+﻿using FluentAssertions;
+using Part2.Models;
+using Part2.Models.Enums;
+using Part2.Services;
+using Part2.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Part2.Tests
+{
+    public class FileComparerTests
+    {
+        private readonly IFileComparer _fileComparer;
+
+        public FileComparerTests()
+        {
+            _fileComparer = new FileComparer();
+        }
+
+        [Fact]
+        public void CompareFiles_ShouldReturnUnchangedPairs()
+        {
+            // Setup
+            var sourceFile = new FileModel
+            {
+                IdValuePairs = new List<KeyValuePair<string, string>>
+                {
+                    new KeyValuePair<string, string>("1", "Value1"),
+                    new KeyValuePair<string, string>("2", "Value2"),
+                    new KeyValuePair<string, string>("3", "Value3")
+                }
+            };
+
+            var targetFile = new FileModel
+            {
+                IdValuePairs = new List<KeyValuePair<string, string>>
+                {
+                    new KeyValuePair<string, string>("1", "Value1"),
+                    new KeyValuePair<string, string>("2", "Value2"),
+                    new KeyValuePair<string, string>("3", "Value3")
+                }
+            };
+
+            // Act
+            var result = _fileComparer.CompareFiles(sourceFile, targetFile);
+
+            // Assert
+            result.Should().HaveCount(3);
+
+            foreach (var res in result)
+            {
+                res.Status.Should().Be(ResultStatusEnum.unchanged);
+            }
+        }
+
+        [Fact]
+        public void CompareFiles_ShouldReturnModifiedPairs()
+        {
+            // Setup
+            var sourceFile = new FileModel
+            {
+                IdValuePairs = new List<KeyValuePair<string, string>>
+                {
+                    new KeyValuePair<string, string>("1", "Value1"),
+                    new KeyValuePair<string, string>("2", "Value2"),
+                    new KeyValuePair<string, string>("3", "Value3")
+                }
+            };
+
+            var targetFile = new FileModel
+            {
+                IdValuePairs = new List<KeyValuePair<string, string>>
+                {
+                    new KeyValuePair<string, string>("1", "Value1"),
+                    new KeyValuePair<string, string>("2", "Value2"),
+                    new KeyValuePair<string, string>("3", "Value5")
+                }
+            };
+
+            // Act
+            var result = _fileComparer.CompareFiles(sourceFile, targetFile);
+
+            // Assert
+            result.Should().HaveCount(3);
+
+            for (var i = 0; i < result.Count - 1; i++)
+            {
+                result.ElementAt(i).Status.Should().Be(ResultStatusEnum.unchanged);
+            }
+
+            result.ElementAt(2).Status.Should().Be(ResultStatusEnum.modified);
+        }
+
+        [Fact]
+        public void CompareFiles_ShouldReturnRemovedPairs()
+        {
+            // Setup
+            var sourceFile = new FileModel
+            {
+                IdValuePairs = new List<KeyValuePair<string, string>>
+                {
+                    new KeyValuePair<string, string>("1", "Value1"),
+                    new KeyValuePair<string, string>("2", "Value2"),
+                    new KeyValuePair<string, string>("3", "Value3")
+                }
+            };
+
+            var targetFile = new FileModel
+            {
+                IdValuePairs = new List<KeyValuePair<string, string>>
+                {
+                    new KeyValuePair<string, string>("1", "Value1"),
+                    new KeyValuePair<string, string>("2", "Value2")
+                }
+            };
+
+            // Act
+            var result = _fileComparer.CompareFiles(sourceFile, targetFile);
+
+            // Assert
+            result.Should().HaveCount(3);
+
+            for (var i = 0; i < result.Count - 1; i++)
+            {
+                result.ElementAt(i).Status.Should().Be(ResultStatusEnum.unchanged);
+            }
+
+            result.ElementAt(result.Count - 1).Status.Should().Be(ResultStatusEnum.removed);
+        }
+
+        [Fact]
+        public void CompareFiles_ShouldReturnAddedPairs()
+        {
+            // Setup
+            var sourceFile = new FileModel
+            {
+                IdValuePairs = new List<KeyValuePair<string, string>>
+                {
+                    new KeyValuePair<string, string>("1", "Value1"),
+                    new KeyValuePair<string, string>("2", "Value2")
+                }
+            };
+
+            var targetFile = new FileModel
+            {
+                IdValuePairs = new List<KeyValuePair<string, string>>
+                {
+                    new KeyValuePair<string, string>("1", "Value1"),
+                    new KeyValuePair<string, string>("2", "Value2"),
+                    new KeyValuePair<string, string>("3", "Value5")
+                }
+            };
+
+            // Act
+            var result = _fileComparer.CompareFiles(sourceFile, targetFile);
+
+            // Assert
+            result.Should().HaveCount(3);
+
+            for (var i = 0; i < result.Count - 1;  i++)
+            {
+                result.ElementAt(i).Status.Should().Be(ResultStatusEnum.unchanged);
+            }
+
+            result.ElementAt(result.Count - 1).Status.Should().Be(ResultStatusEnum.added);
+        }
+    }
+}
---
+++ Exercises/tests/Part2.Tests/FileValidatorTests.cs
@@ -0,0 +1,91 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Moq;
+using Part2.Services;
+using Part2.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Part2.Tests
+{
+    public class FileValidatorTests
+    {
+        private readonly Mock<IConfiguration> _configuration;
+        private readonly IFileValidator _fileValidator;
+        private readonly Mock<IFormFile> _fileMock;
+
+        public FileValidatorTests()
+        {
+            _configuration = new Mock<IConfiguration>();
+            _configuration.Setup(c => c["FileUploadSettings:SizeLimit"]).Returns("1024");
+
+            var fileExtensionSectionMock = new Mock<IConfigurationSection>();
+            fileExtensionSectionMock
+               .Setup(x => x.Value)
+               .Returns(".cfg");
+
+            _configuration.Setup(c => c.GetSection("FileUploadSettings:PermitedExtensions")).Returns(fileExtensionSectionMock.Object);
+
+            _fileMock = new Mock<IFormFile>();
+
+            _fileValidator = new FileValidator(_configuration.Object);
+        }
+
+        [Fact]
+        public void IsValidFileSize_ValidSize_ReturnsTrue()
+        {
+            // Arrange
+            _fileMock.Setup(f => f.Length).Returns(500);
+
+            // Act
+            var result = _fileValidator.IsValidFileSize(_fileMock.Object);
+
+            // Assert
+            result.Should().BeTrue();
+        }
+
+        [Fact]
+        public void IsValidFileSize_InvalidSize_ReturnsFalse()
+        {
+            // Arrange
+            _fileMock.Setup(f => f.Length).Returns(2000);
+
+            // Act
+            var result = _fileValidator.IsValidFileSize(_fileMock.Object);
+
+            // Assert
+            result.Should().BeFalse();
+        }
+
+        [Fact]
+        public void IsValidExtension_ValidExtension_ReturnsTrue()
+        {
+            // Arrange
+            _fileMock.Setup(f => f.FileName).Returns("test.cfg");
+
+            // Act
+            var result = _fileValidator.IsValidFileExtension(_fileMock.Object);
+
+            // Assert
+            result.Should().BeTrue();
+        }
+
+        [Fact]
+        public void IsValidExtension_InvalidExtension_ReturnsFalse()
+        {
+            // Arrange
+            _fileMock.Setup(f => f.FileName).Returns("test.csv");
+
+            // Act
+            var result = _fileValidator.IsValidFileExtension(_fileMock.Object);
+
+            // Assert
+            result.Should().BeFalse();
+        }
+    }
+}
