5d9cb4f2efdc449a880f78ce5700a1e47eaf3691
Astijus Arimavičius <60033715+astijusar@users.noreply.github.com>
2023-07-24 15:59:29 +0300

    Remove material ui

--- Exercises/src/Part3/src/App.jsx
+++ Exercises/src/Part3/src/App.jsx
@@ -1,11 +1,9 @@
 import './css/App.css'
-import Button from '@mui/material/Button';
 
 export default function App() {
   return (
     <>
       <h1>Hello world!</h1>
-      <Button variant="contained">Hello World</Button>
     </>
   )
 }
